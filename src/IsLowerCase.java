91f174370cbd250a663d96e81c98331d3a806e50
kavitha0507 <141806678+kavitha0507@users.noreply.github.com>
2024-11-19 23:57:43 -0600

    Update IsLowerCase.java

--- src/IsLowerCase.java
+++ src/IsLowerCase.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class IsLowerCase {
+    public static void main(String[] args) {
+        String str = "ABCDEFGHI";
+        String s1lower = str.toLowerCase();
+        System.out.println(s1lower);
+    }
 }
