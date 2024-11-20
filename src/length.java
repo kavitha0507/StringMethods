566f53984f92d4afd7d247ae2500c10157bbf020
kavitha0507 <141806678+kavitha0507@users.noreply.github.com>
2024-11-19 23:10:20 -0600

    Update length.java

--- src/length.java
+++ src/length.java
@@ -7,21 +7,8 @@ public class length {
             System.out.println(str2.length());
             System.out.println("Hello World!".length());
             System.out.println("I am learning java".length());
-            class ISEmptyExample {
-                public static void main(String[] args) {
-                    {
-                        String s1 = "";
-                        String s2 = "Good Evening";
-                        System.out.println(s1.isEmpty());
-                        System.out.println(s2.isEmpty());
-
-
-                    }
-
-                }
-
+            
 
             }
         }
-    }
-}
+    }}
