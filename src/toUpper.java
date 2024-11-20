14b73d58640e46cce37079697e78f967c3c6275b
kavitha0507 <141806678+kavitha0507@users.noreply.github.com>
2024-11-20 00:02:20 -0600

    Update toUpper.java

--- src/toUpper.java
+++ src/toUpper.java
@@ -1,4 +1,7 @@
-package PACKAGE_NAME;
-
 public class toUpper {
+    public static void main(String[] args) {
+       String str = "Hello World";
+       System.out.println(str.toUpperCase());
+    }
+
 }
