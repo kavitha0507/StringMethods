11dd9dfe7161fb05dc6fcd33928782c8fc967dcf
kavitha0507 <141806678+kavitha0507@users.noreply.github.com>
2024-11-19 23:59:13 -0600

    Update compare.java

--- src/compare.java
+++ src/compare.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
-
 public class compare {
+    public static void main(String[] args) {
+        String s1 = "kfc";
+        String s2 = "KFC";
+        System.out.println(s1 == s2);
+        String s3 = "Hello!";
+        String s4 = "Hello!";
+        System.out.println(s3 == s4);
+    }
 }
