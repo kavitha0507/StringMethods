11f3a35d1bd9d7262b11b322d04909cdcb58e622
kavitha0507 <141806678+kavitha0507@users.noreply.github.com>
2024-11-19 23:59:59 -0600

    Update concat.java

--- src/concat.java
+++ src/concat.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
-
 public class concat {
+    public static void main(String[] args) {
+        String s1 = "Hi";
+        String s2 = " My name is";
+        System.out.println(s1 +s2);
+         String s3 = " kavi";
+        System.out.println(s2.concat(s3) );
+        String s4 = "Hello";
+        System.out.println(s4.concat(s2).concat(s3));
+    }
 }
