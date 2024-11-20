6916358f7065890a3e175b4560abbd627fda2599
kavitha0507 <141806678+kavitha0507@users.noreply.github.com>
2024-11-20 00:00:53 -0600

    Update indexOf.java

--- src/indexOf.java
+++ src/indexOf.java
@@ -1,4 +1,15 @@
-package PACKAGE_NAME;
-
 public class indexOf {
+    public static void main(String[] args) {
+       String str = "I like Java";
+       // getting index of character 'a'
+       int index = str.indexOf("a");
+       System.out.println(index);
+       int result;
+        result  = str.indexOf("l");
+        System.out.println(result);
+        result = str.lastIndexOf("java");
+        System.out.println(result);
+
+
+    }
 }
