fbbbf001c37a452ab524c8f50a454165c93668d9
kavitha0507 <141806678+kavitha0507@users.noreply.github.com>
2024-11-20 00:01:38 -0600

    Update replace.java

--- src/replace.java
+++ src/replace.java
@@ -1,4 +1,9 @@
-package PACKAGE_NAME;
-
 public class replace {
+    public static void main(String[] args) {
+        String s = "Hello World";
+        System.out.println(s.replace("Hello", "Hi"));
+        System.out.println("world".replace('r','a'));
+        System.out.println(s.replace("Hello World","Good Morning!"));
+
+    }
 }
